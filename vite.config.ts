87f2676074b0f96ef846d149e06cd1b1decb6a0c
Jude Muriithi <jkmuriithi11@gmail.com>
2023-12-02 17:52:30 -0500

    Update readme and add prettify code

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vite';
 
 // Vite config settings go here
-export default defineConfig({});
+export default defineConfig({
+    // Base URL MUST match the name of your repository.
+    base: '/cos426finalproject-vite/',
+});
